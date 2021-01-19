f10216a0ca767a4ba37b5ab8fff2c2f109e311db
Harvinder Singh <77489272+HarvinderVinniSoft@users.noreply.github.com>
2021-01-19 10:25:48 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+  /*
+  
+  $(document).ready(function(){
+    $('#clickHere').click(function(){
+      $('#mySidebar').toggle();
+    });
+  });*/
