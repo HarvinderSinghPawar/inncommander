cf88fb9fd83ace06e84e3f583836aa4bdf89e4ac
Harvinder Singh <77489272+HarvinderVinniSoft@users.noreply.github.com>
2021-01-22 18:49:30 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,16 +1,35 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+  document.getElementById("mySidebar").style.width = "220px";
+  document.getElementById("main").style.marginLeft = "220px";
+}
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+/*
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementByld(headerld)
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
-  /*
-  
-  $(document).ready(function(){
-    $('#clickHere').click(function(){
-      $('#mySidebar').toggle();
-    });
-  });*/
+  if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () =>
+      nav.classList.toggle('show')
+      toggle.classList.toggle('bx-x')
+      bodypd.classList.toggle('body-pd')
+      headerpd.classList.toggle('body-pd')
+    )}
+  }  
+
+  showNavbar('header-toggle','mySidebar','bodypd','header')
+  const linkColor = document.querySelectorAll(.nav__link) 
+
+funcation colorLink(){
+   if(linkColor){
+    linkColor.forEach(l=>l.classList.remove('active'))
+    this.classList.add('active')
+ }  
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
+*/
